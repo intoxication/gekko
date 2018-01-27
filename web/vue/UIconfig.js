e8cff12b74a9c58e99ace5eab33fd7341e134d22
intoxication <intoxication.me@gmail.com>
2018-01-27 18:28:58 +0530

    Update UIconfig.js

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -4,16 +4,22 @@
 // see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
 
 const CONFIG = {
-    headless: true,
-    api: {
-        host: '127.0.0.1',
-        port: 3000,
-    },
-    ui: {
-        ssl: true,
-        host: 'dipiy.herokuapp.com',
-        port: 443,
-        path: 'dipiy.herokuapp.com/gekko' // change this if you are serving from something like `example.com/gekko`
-    },
-    adapter: 'sqlite'
+  headless: false,
+  api: {
+    host: '127.0.0.1',
+    port: 3000,
+    timeout: 120000 // 2 minutes
+  },
+  ui: {
+    ssl: true,
+    host: 'bicointbot.herokuapp.com',
+    port: 443,
+    path: '/'
+  },
+  adapter: 'sqlite'
 }
+
+if(typeof window === 'undefined')
+  module.exports = CONFIG;
+else
+  window.CONFIG = CONFIG;
